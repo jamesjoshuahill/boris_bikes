fb7a0d57753cd30c51a39dae9f1b31c0c9218e04
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-05 12:50:48 +0100

    Continued Garage spec

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,12 +1,31 @@
 class Garage
-  attr_reader :broken_bike_reports
-  
-  def initialize
+  attr_reader :broken_bike_reports, :broken_bikes, :repaired_bikes
+
+  def initialize(van)
+    @van = van
     @broken_bike_reports = [] 
+    @broken_bikes = []
+    @repaired_bikes = []
   end 
 
-  def receive_broken_bike_report (bike_id, station)
+  def receive_broken_bike_report(bike_id, station)
     @broken_bike_reports << [bike_id, station]
+  end
+
+  def send_orders_to_van
+    @broken_bike_reports.each do |report|
+      @van.add_order(report[0], report[1])
+    end
+    @broken_bike_reports = []
+  end
+
+  def receive_broken(bikes)
+    @broken_bikes.concat bikes
+  end
+
+  def fix_broken_bikes
+    @repaired_bikes.concat @broken_bikes
+    @broken_bikes = []
   end
 
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,18 +1,45 @@
 require 'garage'
 
-# should receive broken bike reports
-# should send orders to vans
-# 
-
 describe Garage do
+  let(:van) { double :Van }
+  let(:garage) { Garage.new(van) }
+  let(:station) { double :Station }
+  let(:bike1) { double :Bike }
+  let(:bike2) { double :Bike }
   
   it 'should receive broken bike reports from stations' do
-    garage = Garage.new
-    station = double :Station
     garage.receive_broken_bike_report('1001', station)
     garage.receive_broken_bike_report('1099', station)
     expect(garage.broken_bike_reports).to include ['1001', station]
     expect(garage.broken_bike_reports).to include ['1099', station]
+  end
+
+  it 'should send orders to the van' do
+    garage.receive_broken_bike_report('1001', station)
+    garage.receive_broken_bike_report('1099', station)
+    van.should_receive(:add_order).with('1001', station)
+    van.should_receive(:add_order).with('1099', station)
+    garage.send_orders_to_van
+
+    expect(garage.broken_bike_reports).to be_empty
+  end
+
+  it 'should receive deliveries of broken bikes from the van' do
+    garage.receive_broken([bike1, bike2])
+
+    expect(garage.broken_bikes).to match_array [bike1, bike2]
+  end
+
+  it 'should fix broken bikes' do
+    garage.receive_broken([bike1, bike2])
+    garage.fix_broken_bikes
+
+    expect(garage.broken_bikes).to be_empty
+    expect(garage.repaired_bikes).to match_array [bike1, bike2]
+  end
+
+  xit 'should allocate repaired bikes to stations' do
+
   end
 
 end
